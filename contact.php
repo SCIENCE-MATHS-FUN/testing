dbf61729b9e3732d81b616909d1d8681fca1d829
SCIENCE-MATHS-FUN <83941745+SCIENCE-MATHS-FUN@users.noreply.github.com>
2021-07-25 18:19:47 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+
+if(empty($_POST['submit']))
+{
+echo "Form is not submitted!";
+exit;
+}
+
+if (empty($_POST["fullname"] ) ||
+empty($_POST["gender"] ) ||
+empty($_POST["email"] )
+
+)
+{
+echo "Please Fill the Form";
+exit;
+}
+
+
+$name = $_POST["fullname"];
+$email = $_POST["email"];
+$gender = $_POST["gender"];
+
+
+mail('your-email-id' , 'New Form Submission' , "New Form Submission: Name: $name, Gender: $gender , Email: $email" );
+
+header ('Location: thankyou.html');
+
+
+?>
