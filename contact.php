3753557e0b2fba86caa8f99f9153275511f3dde3
SCIENCE-MATHS-FUN <83941745+SCIENCE-MATHS-FUN@users.noreply.github.com>
2021-07-25 18:20:26 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -22,7 +22,7 @@ $email = $_POST["email"];
 $gender = $_POST["gender"];
 
 
-mail('your-email-id' , 'New Form Submission' , "New Form Submission: Name: $name, Gender: $gender , Email: $email" );
+mail('robin.gamerz77@gmail.com' , 'New Form Submission' , "New Form Submission: Name: $name, Gender: $gender , Email: $email" );
 
 header ('Location: thankyou.html');
 
